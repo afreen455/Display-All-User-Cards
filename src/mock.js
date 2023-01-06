8adbf57d94bd3f878b3a6354b5958c66354c70a2
afreen455 <115209667+afreen455@users.noreply.github.com>
2023-01-06 12:11:20 +0530

    Add files via upload

---
+++ src/mock.js
@@ -0,0 +1,684 @@
+export const users = [
+  {
+    "id": 1,
+    "name": "Lilly Joseph",
+    "username": "Bret",
+    "Gender": "Male",
+    "email": "Lilly@april.biz",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/face-clipart-woman-7.jpg",
+  },
+  {
+    "id": 2,
+    "name": "Howell",
+    "username": "Antonette",
+    "Gender": "Male",
+    "email": "Howell@melissa.tv",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/male-clipart-professional-man-3.jpg",
+  },
+  {
+    "id": 3,
+    "name": "Abbas",
+    "username": "Ali",
+    "Gender": "Male",
+    "email": "abbas123@gmailcom",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user2.jfif",
+  },
+  {
+    "id": 4,
+    "name": "Lebsack",
+    "username": "Karianne",
+    "Gender": "Male",
+    "email": "Julianne@kory.org",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/R-1.jfif",
+  },
+  {
+    "id": 5,
+    "name": "Dietrich",
+    "username": "Kamren",
+    "Gender": "Male",
+    "email": "Hettir@annie.ca",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user5.jfif",
+  },
+  {
+    "id": 6,
+    "name": "Mrs. Dennis",
+    "username": "Leopoldo_Corkery",
+    "Gender": "Male",
+    "email": "Karley@jasper.info",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user6.jfif",
+  },
+  {
+    "id": 7,
+    "name": "Weissnat",
+    "username": "Elwyn.Skiles",
+    "Gender": "Male",
+    "email": "Telly@billy.biz",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/4-47805_cartoon-female-clip-art-teacher-using-technology-clipart.png",
+  },
+  {
+    "id": 8,
+    "name": "Runolfsdottir V",
+    "username": "Maxime_Nienow",
+    "Gender": "Male",
+    "email": "Nicholas@rosamond.me",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user8.jfif",
+  },
+  {
+    "id": 9,
+    "name": "Glenna Reichert",
+    "username": "Delphine",
+    "Gender": "Male",
+    "email": "Chaim_Mc@dana.io",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user2.jfif",
+  },
+  {
+    "id": 10,
+    "name": "Clementina ",
+    "username": "Moriah.Stanton",
+    "Gender": "Male",
+    "email": "Rey.Padberg@yahoo.com",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user4.jfif",
+  },
+  {
+    "id": 11,
+    "name": "Schulist",
+    "username": "Leopoldo_Corkery",
+    "Gender": "Male",
+    "email": "KaryDach@jasper.info",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user6.jfif",
+  },
+  {
+    "id": 12,
+    "name": "Shamoon",
+    "username": "Elwyn.Skiles",
+    "Gender": "Male",
+    "email": "Shamoon@billy.biz",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user7.jfif",
+    "address": {
+      "street": "Rex Trail",
+      "suite": "Suite 280",
+      "city": "Howemouth",
+      "zipcode": "58804-1099",
+    },
+  },
+  {
+
+    "id": 13,
+
+    "name": "Meerub",
+
+    "username": "Dasthari",
+
+    "Gender": "Female",
+
+    "email": "meerub@gmail.com",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/th.jfif",
+
+  },
+
+  {
+
+    "id": 14,
+
+    "name": "Hala",
+
+    "username": "Amir",
+
+    "Gender": "Female",
+
+    "email": "Hala@gmail.com",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/download.jfif",
+
+  },
+
+  {
+
+    "id": 15,
+
+    "name": "Rumy",
+
+    "username": "Khan",
+
+    "Gender": "Female",
+
+    "email": "rumy@mphasis.net",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/user2.jfif",
+
+  },
+
+  {
+
+    "id": 16,
+
+    "name": "Maheer",
+
+    "username": "Ameena",
+
+    "Gender": "Female",
+
+    "email": "maheer@kory.org",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/sandhya.jfif",
+
+  },
+
+  {
+
+    "id": 17,
+
+    "name": "Chelsey ",
+
+    "username": "Kamren",
+
+    "Gender": "Male",
+
+    "email": "Lucio@annie.ca",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/user5.jfif",
+
+  },
+
+  {
+
+    "id": 18,
+
+    "name": "MrDennis",
+
+    "username": "Leopoldo_Corkery",
+
+    "Gender": "Male",
+
+    "email": "Karley@jasper.info",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/user6.jfif",
+
+  },
+
+  {
+
+    "id": 19,
+
+    "name": "Hania",
+
+    "username": "Shaik",
+
+    "Gender": "Female",
+
+    "email": "Hania.Hoeger@billy.biz",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/th.jfif",
+
+  },
+
+  {
+
+    "id": 20,
+
+    "name": "Nicholas ",
+
+    "username": "Maxime_Nienow",
+
+    "Gender": "Male",
+
+    "email": "Nicholas@rosamond.me",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/user8.jfif",
+
+  },
+
+  {
+
+    "id": 21,
+
+    "name": "Wahaj",
+
+    "username": "Khan",
+
+    "Gender": "Male",
+
+    "email": "wahaj@dana.io",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/user2.jfif",
+
+  },
+
+  {
+
+    "id": 22,
+
+    "name": "Feroze khan",
+
+    "username": "Farhad",
+
+    "Gender": "Male",
+
+    "email": "feroze@karina.biz",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/user4.jfif",
+
+  },
+
+  {
+
+    "id": 23,
+
+    "name": "Farhan ",
+
+    "username": "Khan",
+
+    "Gender": "Male",
+
+    "email": "Khan@csv.com",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/user2.jfif",
+
+  },
+  {
+    "id": 24,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "Gender": "Male",
+    "email": "Shanna@melissa.tv",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user_img.jfif",
+  },
+  {
+    "id": 25,
+    "name": "Clementine Bauch",
+    "username": "Samantha",
+    "Gender": "Male",
+    "email": "Nathan@yesenia.net",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user2.jfif",
+  },
+  {
+    "id": 26,
+    "name": "Patricia Lebsack",
+    "username": "Karianne",
+    "Gender": "Male",
+    "email": "Julianne@kory.org",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user4.jfif",
+  },
+  {
+    "id": 27,
+    "name": "Chelsey Dietrich",
+    "username": "Kamren",
+    "Gender": "Male",
+    "email": "Lucio_Hettir@annie.ca",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user5.jfif",
+  },
+  {
+    "id": 28,
+    "name": "Mrs. Dennis Schulist",
+    "username": "Leopoldo_Corkery",
+    "Gender": "Male",
+    "email": "Karley@jasper.info",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user6.jfif",
+  },
+  {
+    "id": 29,
+    "name": "Kurtis Weissnat",
+    "username": "Elwyn.Skiles",
+    "Gender": "Male",
+    "email": "Telly@billy.biz",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user7.jfif",
+  },
+  {
+    "id": 30,
+    "name": "Nicholas Runolfsdottir V",
+    "username": "Maxime_Nienow",
+    "Gender": "Male",
+    "email": "Nicholas@rosamond.me",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user8.jfif",
+  },
+  {
+    "id": 31,
+    "name": "Glenna Reichert",
+    "username": "Delphine",
+    "Gender": "Male",
+    "email": "Chaim_Mc@dana.io",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user2.jfif",
+  },
+  {
+    "id": 32,
+    "name": "Clementina DuBuque",
+    "username": "Moriah.Stanton",
+    "Gender": "Male",
+    "email": "ReyPadberg@yahoo.com",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user4.jfif",
+  },
+  {
+    "id": 33,
+    "name": "Mrs. Dennis Schulist",
+    "username": "Leopoldo_Corkery",
+    "Gender": "Male",
+    "email": "Karley_Dach@jasper.info",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user6.jfif",
+  },
+  {
+
+    "id": 34,
+
+    "name": "Iqra",
+
+    "username": "Aziz",
+
+    "Gender": "Male",
+
+    "email": "iqra@dana.io",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/user2.jfif",
+
+  },
+
+  {
+
+    "id": 35,
+
+    "name": "Aziz",
+
+    "username": "Ahmad",
+
+    "Gender": "Male",
+
+    "email": "Aziz@karina.biz",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/user4.jfif",
+
+  },
+
+  {
+
+    "id": 36,
+
+    "name": "Ismail",
+
+    "username": "Aiman",
+
+    "Gender": "Male",
+
+    "email": "Aiman@csv.com",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/user2.jfif",
+
+  },
+  {
+    "id": 37,
+    "name": "Althaf",
+    "username": "Shaik",
+    "Gender": "Male",
+    "email": "Althaf@gmail.com",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user_img.jfif",
+  },
+  {
+    "id": 38,
+    "name": "Agha Ali",
+    "username": "Samantha",
+    "Gender": "Male",
+    "email": "Sam@yesenia.net",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user2.jfif",
+  },
+  {
+    "id": 39,
+    "name": "Parthu",
+    "username": "Karianne",
+    "Gender": "Male",
+    "email": "Parthu@kory.org",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user4.jfif",
+  },
+  {
+    "id": 40,
+    "name": "derick",
+    "username": "Kamren",
+    "Gender": "Male",
+    "email": "Derick@annie.ca",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user5.jfif",
+  },
+  {
+    "id": 41,
+    "name": "Raghav",
+    "username": "Leopoldo_Corkery",
+    "Gender": "Male",
+    "email": "Raghav@jasper.info",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user6.jfif",
+  },
+  {
+    "id": 42,
+    "name": "Mathew",
+    "username": "Elwyn.Skiles",
+    "Gender": "Male",
+    "email": "Mathew@billy.biz",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user7.jfif",
+  },
+  {
+    "id": 43,
+    "name": "Charles",
+    "username": "Maxime_Nienow",
+    "Gender": "Male",
+    "email": "Charles@rosamond.me",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user8.jfif",
+  },
+  {
+    "id": 44,
+    "name": "Rithivk",
+    "username": "Delphine",
+    "Gender": "Male",
+    "email": "Rithivk@dana.io",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user2.jfif",
+  },
+  {
+    "id": 45,
+    "name": "Tommy Cruz",
+    "username": "Moriah.Stanton",
+    "Gender": "Male",
+    "email": "Tommy@yahoo.com",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user4.jfif",
+  },
+  {
+    "id": 46,
+    "name": "Ali Abbas",
+    "username": "Leopoldo_Corkery",
+    "Gender": "Male",
+    "email": "abbas@jasper.info",
+    "status": "active",
+    "status_class": 'active',
+    "image": "../images/user6.jfif",
+  },
+  {
+
+    "id": 47,
+
+    "name": "Ahmed",
+
+    "username": "Bandaru",
+
+    "Gender": "Female",
+
+    "email": "Ahmed@billy.biz",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/th.jfif",
+
+  },
+  {
+
+    "id": 48,
+
+    "name": "Rimsha",
+
+    "username": "Rimsha",
+
+    "Gender": "Female",
+
+    "email": "Rimsha@gmail.com",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/download.jfif",
+
+  },
+
+  {
+
+    "id": 49,
+
+    "name": "Alishba",
+
+    "username": "alishba",
+
+    "Gender": "Female",
+
+    "email": "Alishba@abc.net",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/user2.jfif",
+
+  },
+
+  {
+
+    "id": 50,
+
+    "name": "Aleena",
+
+    "username": "Aleena Khan",
+
+    "Gender": "Female",
+
+    "email": "aleena@kory.org",
+
+    "status": "active",
+
+    "status_class": 'active',
+
+    "image": "../images/sandhya.jfif",
+
+  },
+]
